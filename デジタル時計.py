ee4f7ba5e4c490a0ec810f82e008f65f9774aa69
ひかる <155365058+miruky@users.noreply.github.com>
2024-01-03 17:34:11 +0900

    Update デジタル時計.py

--- デジタル時計.py
+++ デジタル時計.py
@@ -1,16 +1,1 @@
-from tkinter import *
-from time import strftime
 
-root = Tk()
-root.title("デジタル時計")
-
-def time():
-    clock = strftime('%H:%M:%S %p')
-    label.config(text=clock)
-    label.after(1000,time)
-
-label = Label(root,font=("impact",80,'bold'), bg ='blue',fg="black")
-label.pack(anchor='center')
-time()
-
-root.mainloop()
